81b77a1ef2a15d55705bde91494e508cd2a0760d
DarianA-KingA <86999768+DarianA-KingA@users.noreply.github.com>
2023-04-27 17:35:02 -0400

    Agregar archivos de proyecto.

---
+++ Mango.Services.ProductAPI/Context/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext>options):base(options)
+        {
+            
+        }
+        
+    }
+}
