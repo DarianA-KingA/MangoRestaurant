55ed58199f6b81ae58bd6e22d8b74a5fabb56b51
DarianA-KingA <86999768+DarianA-KingA@users.noreply.github.com>
2023-05-30 10:18:04 -0400

    coupon api done

---
+++ Mango.Service.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,37 @@
+﻿using Mango.Service.CouponAPI.Models.DTO;
+using Mango.Service.CouponAPI.Respository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Service.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : Controller
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDto _response;
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            this._response = new();
+        }
+        [HttpGet("{code}")]
+        [Authorize]
+        public async Task<object> GetDiscountForCode(string CouponCode)
+        {
+            try
+            {
+                CouponDto couponDto = await _couponRepository.GetCouponByCodeAsync(CouponCode);
+                _response.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+    }
+}
---
+++ Mango.Service.CouponAPI/Respository/ICouponRepository.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Service.CouponAPI.Models.DTO;
+
+namespace Mango.Service.CouponAPI.Respository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCodeAsync(string couponCode);
+
+
+    }
+}
