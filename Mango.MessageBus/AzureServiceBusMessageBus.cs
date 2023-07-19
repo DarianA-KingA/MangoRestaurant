8add0f566e95f1190388639250444027578bfa2d
DarianA-KingA <86999768+DarianA-KingA@users.noreply.github.com>
2023-07-19 09:34:26 -0400

    Message Bus Sender Implementation

---
+++ Mango.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    internal class AzureServiceBusMessageBus : IMessageBus
+    {
+        //can be improved
+        private string connectionString = "Endpoint=sb://mangorestaurantmicroservicebusservice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=TLJEsOWmj4BEqvOS1ADCC1pWlykZMGHYN+ASbNrqhEM=";
+        public async Task PublishMessage(BaseMessage message, string topicName)
+        {
+            ISenderClient senderClient = new TopicClient(connectionString, topicName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+
+            var finalMessage = new Message(Encoding.UTF8.GetBytes(jsonMessage)) { 
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+            await senderClient.SendAsync(finalMessage);
+
+            await senderClient.CloseAsync();
+        }
+    }
+}
